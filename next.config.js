28ad7dec58b4036e2176c94b2e453aa679f07d9d
Syam Sasi <syamsasi99@gmail.com>
2025-10-08 00:34:07 +0800

    adding index

--- next.config.js
+++ next.config.js
@@ -3,10 +3,12 @@ const { withContentlayer } = require('next-contentlayer')
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
 const assetPrefix = process.env.NEXT_PUBLIC_ASSET_PREFIX || ''
 
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
-  output: 'export',
-  basePath: '/taqelah-site',
-  assetPrefix: '/taqelah-site/',
+  output: isProd ? 'export' : undefined,
+  basePath: isProd ? '/taqelah-site' : '',
+  assetPrefix: isProd ? '/taqelah-site/' : '',
   images: {
     unoptimized: true,
   },
